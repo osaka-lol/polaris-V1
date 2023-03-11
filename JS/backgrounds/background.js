fe15fbce4aebd87db8ae24fda3a7068662e19b82
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-11 13:09:32 -0500

    Update background.js

--- JS/backgrounds/background.js
+++ JS/backgrounds/background.js
@@ -1,14 +1,1 @@
-  const cssSelector = document.getElementById("css-selector");
-  cssSelector.addEventListener("change", function() {
-    const selectedCss = cssSelector.value;
-    const linkTag = document.getElementById("custom-css");
-    linkTag.href = selectedCss;
-    localStorage.setItem("selectedCss", selectedCss);
-  });
 
-  const storedCss = localStorage.getItem("selectedCss");
-  if (storedCss) {
-    const linkTag = document.getElementById("custom-css");
-    linkTag.href = storedCss;
-    cssSelector.value = storedCss;
-  }

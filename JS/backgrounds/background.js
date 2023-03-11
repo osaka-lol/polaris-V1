a6ad545f29ec17d3b19d1e1c06b821a45f3622d7
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-11 16:00:02 -0500

    Update background.js

--- JS/backgrounds/background.js
+++ JS/backgrounds/background.js
@@ -1,1 +1,17 @@
+const selectElement = document.querySelector('#style-select');
+const styleElement = document.querySelector('#style');
 
+// Check if a stylesheet preference has been saved in local storage
+const savedStyle = localStorage.getItem('style');
+if (savedStyle) {
+  styleElement.setAttribute('href', savedStyle);
+  selectElement.value = savedStyle;
+}
+
+selectElement.addEventListener('change', (event) => {
+  const selectedOption = event.target.value;
+  styleElement.setAttribute('href', selectedOption);
+  
+  // Save the selected option to local storage
+  localStorage.setItem('style', selectedOption);
+});

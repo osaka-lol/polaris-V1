2c8d923d051bb56093c43a5eeece633d0285e4f3
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-23 19:47:36 -0400

    Update theme.js

--- js/themes/theme.js
+++ js/themes/theme.js
@@ -1,16 +1,22 @@
-// define a function to change the CSS file
 function changeStyle(style) {
   var styleLink = document.getElementById("style-link");
   styleLink.href = style;
+  localStorage.setItem("selectedStyle", style);
 }
 
 // add click event listeners to the buttons
-var button1 = document.getElementById("creamsicle");
+var button1 = document.getElementById("button1");
 button1.addEventListener("click", function() {
   changeStyle("/css/themes/theme-purple-orange.css");
 });
 
-var button2 = document.getElementById("grey");
+var button2 = document.getElementById("button2");
 button2.addEventListener("click", function() {
   changeStyle("/css/themes/theme-grey.css");
 });
+
+// load selected style from local storage
+var selectedStyle = localStorage.getItem("selectedStyle");
+if (selectedStyle) {
+  changeStyle(selectedStyle);
+}

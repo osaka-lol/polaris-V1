f1ad45968649e40583edc49ac62fbdd27d453410
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-23 19:49:54 -0400

    Update theme.js

--- js/themes/theme.js
+++ js/themes/theme.js
@@ -5,13 +5,13 @@ function changeStyle(style) {
 }
 
 // add click event listeners to the buttons
-var button1 = document.getElementById("button1");
-button1.addEventListener("click", function() {
+var button1 = document.getElementById("theme1");
+theme1.addEventListener("click", function() {
   changeStyle("/css/themes/theme-purple-orange.css");
 });
 
-var button2 = document.getElementById("button2");
-button2.addEventListener("click", function() {
+var button2 = document.getElementById("theme2");
+theme2.addEventListener("click", function() {
   changeStyle("/css/themes/theme-grey.css");
 });
 

a3517d814b5493b31d641d6a429f0a00118799ab
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-24 20:12:17 -0400

    Update theme.js

--- js/themes/theme.js
+++ js/themes/theme.js
@@ -1,22 +1,13 @@
-function changeStyle(style) {
-  var styleLink = document.getElementById("style-link");
-  styleLink.href = style;
-  localStorage.setItem("selectedStyle", style);
+function changeStylesheet(newStylesheet) {
+  var stylesheet = document.getElementById("stylesheet");
+  stylesheet.setAttribute("href", newStylesheet);
+  localStorage.setItem("selectedStylesheet", newStylesheet);
 }
 
-// add click event listeners to the buttons
-var theme1 = document.getElementById("theme1");
-theme1.addEventListener("click", function() {
-  changeStyle("/css/themes/theme-purple-orange.css");
+window.addEventListener('load', function() {
+  var selectedStylesheet = localStorage.getItem("selectedStylesheet");
+  if (selectedStylesheet) {
+    var stylesheet = document.getElementById("stylesheet");
+    stylesheet.setAttribute("href", selectedStylesheet);
+  }
 });
-
-var theme2 = document.getElementById("theme2");
-theme2.addEventListener("click", function() {
-  changeStyle("/css/themes/theme-grey.css");
-});
-
-// load selected style from local storage
-var selectedStyle = localStorage.getItem("selectedStyle");
-if (selectedStyle) {
-  changeStyle(selectedStyle);
-}

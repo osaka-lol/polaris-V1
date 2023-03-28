7804350bd35047952a498df42bc6810ee01f7db6
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-28 13:45:48 -0400

    Update tabcloak.js

--- js/settings/tabcloak.js
+++ js/settings/tabcloak.js
@@ -1,3 +1,15 @@
+/*
+Hello epic hacker (maybe skid) you are looking at one of the many scripts that powers the site,
+this script has extra comments and info to help you understand what is going on.
+This JavaScript code defines functions that manage the state of a web page's tab,
+such as its title and icon, and its theme.
+It retrieves a JSON object from local storage, which contains the current state of the tab,
+and updates the web page's elements with the stored values.
+The code provides functions to modify the tab state and settings, such as setTitle, setFavicon,
+resetTab, setTheme, and setThemeColor.
+These functions update the web page's elements and store the updated state in local storage.
+*/
+
 // Check if there is a saved tab data in localStorage
 var tab = localStorage.getItem("tab");
 
@@ -24,8 +36,8 @@ if (tabData.icon) {
 
 // Default tab settings
 var settingsDefaultTab = {
-  title: "Polaris",
-  icon: "",
+  title: "Settings | 3kh0",
+  icon: "./images/logo.png",
 };
 
 // Function to set the document title
@@ -157,5 +169,63 @@ function setCloak(cloak) { // applies only to premade cloaks
       setFavicon("");
       location.reload();
       break;
+  }
+}
+
+// Function to reset the tab settings to default
+function resetTab() {
+  document.title = settingsDefaultTab.title;
+  document.querySelector("link[rel='icon']").href = settingsDefaultTab.icon;
+  document.getElementById("title").value = "";
+  document.getElementById("icon").value = "";
+  localStorage.setItem("tab", JSON.stringify({}));
+}
+
+// Function to set the theme
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+  document.body.setAttribute("theme", theme);
+  document.body.style = "";
+  localStorage.removeItem("theme_color");
+
+  // Find the theme color from the themes array and set the color
+  themes.forEach((palette) => {
+    if (palette.theme == theme) {
+      document.querySelector("#theme_color").value = palette.color;
+    }
+  });
+}
+
+// Function to set the custom theme color
+function setThemeColor(theme) {
+  localStorage.setItem("theme", "custom");
+  localStorage.setItem("theme_color", theme);
+  document.body.setAttribute("theme", "custom");
+  document.body.style = `--theme: ${theme}; --background: ${getContrastHex(theme)}; --text: ${getColorHex(theme)}; --text-secondary: ${getColorHex(theme)};`;
+}
+// =======================
+/*
+More custom cloak tomfoolery(searchable dropdown)
+*/
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showccdrop() { // show
+  document.getElementById("ccdrop").classList.toggle("show");
+}
+// the below has been stolen from w3schools
+function search() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("ccsearch"); // the search bar in the dropdown
+  filter = input.value.toUpperCase();
+  div = document.getElementById("ccdrop"); // dropdown(self-explainatory)
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
   }
 }

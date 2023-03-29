b7baed778c497e27e06c57fba140b7018555addd
Cobalt-60 <95923271+thetatertotgod@users.noreply.github.com>
2023-03-28 22:54:41 -0400

    Update tabcloak.js

--- js/sidebar/tabcloak.js
+++ js/sidebar/tabcloak.js
@@ -1,13 +1,13 @@
 // Get the buttons and add event listeners to them
 var driveBtn = document.getElementById("drive-btn");
 var canvasBtn = document.getElementById("canvas-btn");
-var classroomBtn = document.getElementById("classroom-btn");
+var gmailBtn = document.getElementById("gmail-btn");
 var googleBtn = document.getElementById("google-btn");
 
 driveBtn.addEventListener("click", function() {
   // Change the title and icon
   document.title = "Google Drive";
-  document.querySelector('link[rel="shortcut icon"]').href = ""; // put file path for icon here
+  document.querySelector('link[rel="shortcut icon"]').href = "/assets/fonts/icons/Drive.ico"; // put file path for icon here
   // Save the choice to local storage
   localStorage.setItem("websiteTitle", document.title);
   localStorage.setItem("websiteIcon", ""); // put file path for icon here
@@ -16,16 +16,16 @@ driveBtn.addEventListener("click", function() {
 canvasBtn.addEventListener("click", function() {
   // Change the title and icon
   document.title = "Canvas";
-  document.querySelector('link[rel="shortcut icon"]').href = ""; // put file path for icon here
+  document.querySelector('link[rel="shortcut icon"]').href = "/assets/fonts/icons/Canvas.ico"; // put file path for icon here
   // Save the choice to local storage
   localStorage.setItem("websiteTitle", document.title);
   localStorage.setItem("websiteIcon", ""); // put file path for icon here
 });
 
-classroomBtn.addEventListener("click", function() {
+gmailBtn.addEventListener("click", function() {
   // Change the title and icon
-  document.title = "Google Classroom";
-  document.querySelector('link[rel="shortcut icon"]').href = ""; // put file path for icon here
+  document.title = "Gmail";
+  document.querySelector('link[rel="shortcut icon"]').href = "/assets/fonts/icons/Gmail.ico"; // put file path for icon here
   // Save the choice to local storage
   localStorage.setItem("websiteTitle", document.title);
   localStorage.setItem("websiteIcon", ""); // put file path for icon here

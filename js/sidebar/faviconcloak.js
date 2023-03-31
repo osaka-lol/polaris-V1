2395aa7fe22696147a61f9969b383b407c7c82bc
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-03-30 21:57:38 -0400

    Update faviconcloak.js

--- js/sidebar/faviconcloak.js
+++ js/sidebar/faviconcloak.js
@@ -1,4 +1,5 @@
-var selectedFavicon = localStorage.getItem("selectedFavicon");
-    if (selectedFavicon) {
-      changeFavicon(selectedFavicon);
-    }
+function changeFavicon(iconLink) {
+  var link = document.getElementById("favicon");
+  link.href = iconLink;
+  localStorage.setItem("selectedFavicon", iconLink);
+}

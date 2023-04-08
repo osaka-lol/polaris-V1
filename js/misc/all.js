a693920b1ab56b67b3a1b3efda3eece26a2a0d7d
Blazer <84545189+beforeblazergithub@users.noreply.github.com>
2023-04-07 20:27:15 -0500

    add loader

--- js/misc/all.js
+++ js/misc/all.js
@@ -1,1 +1,8 @@
 $('#loadnav').load(`${location.protocol}//${location.host}/html/nav.html`);
+
+setTimeout(function() {
+    document.getElementById('loader').classList.add('hide');
+    setTimeout(function() {
+        document.getElementById('loader').style.display = 'none';
+    }, 1000);
+}, 3000);
